joshfire/chromiumos-platform-window_manager

commit 98b6cb5c2de9a32f4edafc9581d1ec0429a21087

combine texture and pixmap prototype
Review URL: http://codereview.chromium.org/2872023

gles/opengles_visitor.h
@@ -15,6 +15,7 @@
 #include "window_manager/compositor.h"
 #include "window_manager/math_types.h"
 #include "window_manager/real_compositor.h"
+#include "window_manager/texture_data.h"
 
 namespace window_manager {
 
@@ -28,12 +29,6 @@ class Gles2Interface;
 // This class vists an actor tree and draws it using OpenGLES
 class OpenGlesDrawVisitor : virtual public RealCompositor::ActorVisitor {
  public:
-  // IDs for storing drawing data
-  enum DataId {
-    kTextureData = 1,
-    kEglImageData
-  };
-
   OpenGlesDrawVisitor(Gles2Interface* gl,
                       RealCompositor* compositor,
                       Compositor::StageActor* stage);
@@ -78,30 +73,22 @@ class OpenGlesDrawVisitor : virtual public RealCompositor::ActorVisitor {
   DISALLOW_COPY_AND_ASSIGN(OpenGlesDrawVisitor);
 };
 
-class OpenGlesTextureData : public RealCompositor::DrawingData {
+// TODO: further combine texture class between GL and GLES after common GL
+// functions are combined.
+class OpenGlesTextureData : public TextureData {
  public:
   explicit OpenGlesTextureData(Gles2Interface* gl);
   virtual ~OpenGlesTextureData();
 
-  void SetTexture(GLuint texture, bool has_alpha);
-
-  GLuint texture() const { return texture_; }
-  bool has_alpha() const { return has_alpha_; }
+  void SetTexture(GLuint texture);
 
  private:
   Gles2Interface* gl_;  // Not owned.
 
-  // Texture ID of the wrapped texture; this takes ownership of the texture
-  // handle
-  GLuint texture_;
-
-  // Does this texture require alpha-blending?
-  bool has_alpha_;
-
   DISALLOW_COPY_AND_ASSIGN(OpenGlesTextureData);
 };
 
-class OpenGlesEglImageData : public RealCompositor::DrawingData {
+class OpenGlesEglImageData : public DynamicTextureData {
  public:
   OpenGlesEglImageData(Gles2Interface* gl);
   virtual ~OpenGlesEglImageData();
@@ -115,9 +102,6 @@ class OpenGlesEglImageData : public RealCompositor::DrawingData {
 
   // Create and bind a GL texture
   void BindTexture(OpenGlesTextureData* texture, bool has_alpha);
-
-  // Respond to damage events
-  void Refresh() {}
 
  private:
   // Has Bind() returned successfully

compositor.h
@@ -19,6 +19,7 @@
 namespace window_manager {
 
 class CompositorEventSource;
+class ImageContainer;
 template<class T> class Stacker;  // from util.h
 class XConnection;
 
@@ -128,6 +129,17 @@ class Compositor {
     DISALLOW_COPY_AND_ASSIGN(StageActor);
   };
 
+  // ImageActor displays static image onscreen.
+  class ImageActor : virtual public Actor {
+   public:
+    ImageActor() {}
+    virtual ~ImageActor() {}
+
+    virtual void SetImage(const ImageContainer& image_container) = 0;
+   private:
+    DISALLOW_COPY_AND_ASSIGN(ImageActor);
+  };
+
   // TexturePixmapActor displays the contents of a pixmap onscreen.
   class TexturePixmapActor : virtual public Actor {
    public:
@@ -159,7 +171,7 @@ class Compositor {
   virtual ContainerActor* CreateGroup() = 0;
   virtual Actor* CreateRectangle(const Color& color, const Color& border_color,
                                  int border_width) = 0;
-  virtual Actor* CreateImage(const std::string& filename) = 0;
+  virtual ImageActor* CreateImage(const std::string& filename) = 0;
   virtual TexturePixmapActor* CreateTexturePixmap() = 0;
   virtual Actor* CreateText(const std::string& font_name,
                             const std::string& text,
@@ -336,6 +348,19 @@ class MockCompositor : public Compositor {
     DISALLOW_COPY_AND_ASSIGN(StageActor);
   };
 
+  class ImageActor : public MockCompositor::Actor,
+                     public Compositor::ImageActor {
+   public:
+    ImageActor() {}
+    virtual ~ImageActor() {}
+
+    // Begin Compositor::ImageActor methods.
+    virtual void SetImage(const ImageContainer& image_container) {}
+    // End Compositor::ImageActor methods.
+   private:
+    DISALLOW_COPY_AND_ASSIGN(ImageActor);
+  };
+
   class TexturePixmapActor : public MockCompositor::Actor,
                              public Compositor::TexturePixmapActor {
    public:
@@ -390,7 +415,9 @@ class MockCompositor : public Compositor {
                                  int border_width) {
     return new Actor;
   }
-  virtual Actor* CreateImage(const std::string& filename) { return new Actor; }
+  virtual ImageActor* CreateImage(const std::string& filename) {
+    return new ImageActor;
+  }
   virtual TexturePixmapActor* CreateTexturePixmap() {
     return new TexturePixmapActor(xconn_);
   }

gles/opengles_visitor.cc
@@ -131,9 +131,10 @@ void OpenGlesDrawVisitor::BindImage(const ImageContainer* container,
                   0, GL_RGBA, GL_UNSIGNED_BYTE, container->data());
 
   OpenGlesTextureData* data = new OpenGlesTextureData(gl_);
-  data->SetTexture(texture,
-                   container->format() == ImageContainer::IMAGE_FORMAT_RGBA_32);
-  actor->SetDrawingData(kTextureData, RealCompositor::DrawingDataPtr(data));
+  data->SetTexture(texture);
+  data->set_has_alpha(
+      container->format() == ImageContainer::IMAGE_FORMAT_RGBA_32);
+  actor->set_texture_data(data);
   LOG(INFO) << "Binding image " << container->filename()
             << " to texture " << texture;
 }
@@ -188,26 +189,18 @@ void OpenGlesDrawVisitor::VisitTexturePixmap(
   if (!actor->IsVisible())
     return;
 
-  OpenGlesEglImageData* image_data = dynamic_cast<OpenGlesEglImageData*>(
-      actor->GetDrawingData(kEglImageData).get());
+  if (!actor->texture_data()) {
+    OpenGlesEglImageData image_data(gl_);
 
-  if (!image_data) {
-    image_data = new OpenGlesEglImageData(gl_);
-    actor->SetDrawingData(kEglImageData,
-                          RealCompositor::DrawingDataPtr(image_data));
+    if (!image_data.Bind(actor, egl_context_))
+      return;
+
+    OpenGlesTextureData* texture = new OpenGlesTextureData(gl_);
+    image_data.BindTexture(texture, !actor->pixmap_is_opaque());
+    actor->set_texture_data(texture);
   }
 
-  if (!image_data->bound()) {
-    if (image_data->Bind(actor, egl_context_)) {
-      OpenGlesTextureData* texture = new OpenGlesTextureData(gl_);
-      image_data->BindTexture(texture, actor->pixmap_is_opaque() == false);
-      actor->SetDrawingData(kTextureData,
-                            RealCompositor::DrawingDataPtr(texture));
-      VisitQuad(actor);
-    }
-  } else {
-    VisitQuad(actor);
-  }
+  VisitQuad(actor);
 }
 
 void OpenGlesDrawVisitor::VisitQuad(RealCompositor::QuadActor* actor) {
@@ -221,9 +214,9 @@ void OpenGlesDrawVisitor::VisitQuad(RealCompositor::QuadActor* actor) {
   Matrix4 mvp = projection_ * actor->model_view();
 
   // texture
-  OpenGlesTextureData* texture_data = dynamic_cast<OpenGlesTextureData*>(
-      actor->GetDrawingData(kTextureData).get());
-  gl_->BindTexture(GL_TEXTURE_2D, texture_data ? texture_data->texture() : 0);
+  TextureData* texture_data = actor->texture_data();
+  gl_->BindTexture(GL_TEXTURE_2D,
+                   texture_data ? texture_data->texture() : 0);
   const bool texture_has_alpha = texture_data ?
                                    texture_data->has_alpha() :
                                    true;
@@ -345,18 +338,15 @@ void OpenGlesDrawVisitor::VisitContainer(
 }
 
 OpenGlesTextureData::OpenGlesTextureData(Gles2Interface* gl)
-    : gl_(gl),
-      texture_(0),
-      has_alpha_(false) {}
+    : gl_(gl) {}
 
 OpenGlesTextureData::~OpenGlesTextureData() {
-  gl_->DeleteTextures(1, &texture_);
+  gl_->DeleteTextures(1, texture_ptr());
 }
 
-void OpenGlesTextureData::SetTexture(GLuint texture, bool has_alpha) {
-  gl_->DeleteTextures(1, &texture_);
-  texture_ = texture;
-  has_alpha_ = has_alpha;
+void OpenGlesTextureData::SetTexture(GLuint texture) {
+  gl_->DeleteTextures(1, texture_ptr());
+  set_texture(texture);
 }
 
 OpenGlesEglImageData::OpenGlesEglImageData(Gles2Interface* gl)
@@ -417,7 +407,8 @@ void OpenGlesEglImageData::BindTexture(OpenGlesTextureData* texture_data,
   gl_->EGLImageTargetTexture2DOES(GL_TEXTURE_2D,
                                   static_cast<GLeglImageOES>(egl_image_));
 
-  texture_data->SetTexture(texture, has_alpha);
+  texture_data->SetTexture(texture);
+  texture_data->set_has_alpha(has_alpha);
 }
 
 }  // namespace window_manager

opengl_visitor.cc
@@ -46,84 +46,16 @@ using window_manager::util::XidStr;
 
 namespace window_manager {
 
-OpenGlDrawVisitor::OpenGlStateCache::OpenGlStateCache() {
-  Invalidate();
-}
-
-void OpenGlDrawVisitor::OpenGlStateCache::Invalidate() {
-  actor_opacity_ = -1.f;
-  dimmed_transparency_ = -1.f;
-  red_ = -1.f;
-  green_ = -1.f;
-  blue_ = -1.f;
-}
-
-bool OpenGlDrawVisitor::OpenGlStateCache::ColorStateChanged(
-    float actor_opacity, float dimmed_transparency,
-    float red, float green, float blue) {
-  if (actor_opacity != actor_opacity_ ||
-      dimmed_transparency != dimmed_transparency_ ||
-      red != red_ || green != green_ || blue != blue_) {
-    actor_opacity_ = actor_opacity;
-    dimmed_transparency_ = dimmed_transparency;
-    red_ = red;
-    green_ = green;
-    blue_ = blue;
-    return true;
-  }
-  return false;
-}
-
-OpenGlQuadDrawingData::OpenGlQuadDrawingData(GLInterface* gl_interface)
-    : gl_interface_(gl_interface) {
-  gl_interface_->GenBuffers(1, &vertex_buffer_);
-  gl_interface_->BindBuffer(GL_ARRAY_BUFFER, vertex_buffer_);
-
-  static const float kQuad[] = {
-    0.f, 0.f,
-    0.f, 1.f,
-    1.f, 0.f,
-    1.f, 1.f,
-  };
-
-  gl_interface_->BufferData(GL_ARRAY_BUFFER, sizeof(kQuad),
-                            kQuad, GL_STATIC_DRAW);
-  color_buffer_.reset(new float[4*4]);
-  CHECK_GL_ERROR(gl_interface_);
-}
-
-OpenGlQuadDrawingData::~OpenGlQuadDrawingData() {
-  if (vertex_buffer_)
-    gl_interface_->DeleteBuffers(1, &vertex_buffer_);
-}
-
-void OpenGlQuadDrawingData::set_vertex_color(int index,
-                                             float r,
-                                             float g,
-                                             float b,
-                                             float a) {
-  // Calculate member index
-  index *= 4;
-  // Assign the color.
-  color_buffer_[index++] = r;
-  color_buffer_[index++] = g;
-  color_buffer_[index++] = b;
-  color_buffer_[index] = a;
-}
-
 OpenGlPixmapData::OpenGlPixmapData(OpenGlDrawVisitor* visitor)
     : visitor_(visitor),
       gl_(visitor_->gl_interface_),
-      texture_(0),
       glx_pixmap_(0) {
   DCHECK(visitor);
 }
 
 OpenGlPixmapData::~OpenGlPixmapData() {
-  if (texture_) {
-    gl_->DeleteTextures(1, &texture_);
-    texture_ = 0;
-  }
+  if (texture())
+    gl_->DeleteTextures(1, texture_ptr());
   if (glx_pixmap_) {
     gl_->DestroyGlxPixmap(glx_pixmap_);
     glx_pixmap_ = 0;
@@ -163,8 +95,9 @@ bool OpenGlPixmapData::Init(RealCompositor::TexturePixmapActor* actor) {
     return false;
   }
 
-  gl_->GenTextures(1, &texture_);
-  gl_->BindTexture(GL_TEXTURE_2D, texture_);
+  GLuint new_texture = 0;
+  gl_->GenTextures(1, &new_texture);
+  gl_->BindTexture(GL_TEXTURE_2D, new_texture);
   gl_->EnableAnisotropicFiltering();
   gl_->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   gl_->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
@@ -172,35 +105,97 @@ bool OpenGlPixmapData::Init(RealCompositor::TexturePixmapActor* actor) {
   gl_->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   gl_->BindGlxTexImage(glx_pixmap_, GLX_FRONT_LEFT_EXT, NULL);
   CHECK_GL_ERROR(gl_);
+  set_texture(new_texture);
   return true;
 }
 
 void OpenGlPixmapData::Refresh() {
-  DCHECK(texture_);
+  DCHECK(texture());
   DCHECK(glx_pixmap_);
-  gl_->BindTexture(GL_TEXTURE_2D, texture_);
+  gl_->BindTexture(GL_TEXTURE_2D, texture());
   gl_->ReleaseGlxTexImage(glx_pixmap_, GLX_FRONT_LEFT_EXT);
   gl_->BindGlxTexImage(glx_pixmap_, GLX_FRONT_LEFT_EXT, NULL);
   CHECK_GL_ERROR(gl_);
 }
 
 OpenGlTextureData::OpenGlTextureData(GLInterface* gl_interface)
-    : gl_interface_(gl_interface),
-      texture_(0),
-      has_alpha_(false) {}
+    : gl_interface_(gl_interface) {}
 
 OpenGlTextureData::~OpenGlTextureData() {
-  if (texture_) {
-    gl_interface_->DeleteTextures(1, &texture_);
-  }
+  if (texture())
+    gl_interface_->DeleteTextures(1, texture_ptr());
 }
 
-void OpenGlTextureData::SetTexture(GLuint texture, bool has_alpha) {
-  if (texture_ && texture_ != texture) {
-    gl_interface_->DeleteTextures(1, &texture_);
+void OpenGlTextureData::SetTexture(GLuint texture) {
+  if (this->texture() && this->texture() != texture)
+    gl_interface_->DeleteTextures(1, texture_ptr());
+  set_texture(texture);
+}
+
+OpenGlDrawVisitor::OpenGlQuadDrawingData::OpenGlQuadDrawingData(
+    GLInterface* gl_interface)
+    : gl_interface_(gl_interface) {
+  gl_interface_->GenBuffers(1, &vertex_buffer_);
+  gl_interface_->BindBuffer(GL_ARRAY_BUFFER, vertex_buffer_);
+
+  static const float kQuad[] = {
+    0.f, 0.f,
+    0.f, 1.f,
+    1.f, 0.f,
+    1.f, 1.f,
+  };
+
+  gl_interface_->BufferData(GL_ARRAY_BUFFER, sizeof(kQuad),
+                            kQuad, GL_STATIC_DRAW);
+  color_buffer_.reset(new float[4*4]);
+  CHECK_GL_ERROR(gl_interface_);
+}
+
+OpenGlDrawVisitor::OpenGlQuadDrawingData::~OpenGlQuadDrawingData() {
+  if (vertex_buffer_)
+    gl_interface_->DeleteBuffers(1, &vertex_buffer_);
+}
+
+void OpenGlDrawVisitor::OpenGlQuadDrawingData::set_vertex_color(int index,
+                                                                float r,
+                                                                float g,
+                                                                float b,
+                                                                float a) {
+  // Calculate member index
+  index *= 4;
+  // Assign the color.
+  color_buffer_[index++] = r;
+  color_buffer_[index++] = g;
+  color_buffer_[index++] = b;
+  color_buffer_[index] = a;
+}
+
+OpenGlDrawVisitor::OpenGlStateCache::OpenGlStateCache() {
+  Invalidate();
+}
+
+void OpenGlDrawVisitor::OpenGlStateCache::Invalidate() {
+  actor_opacity_ = -1.f;
+  dimmed_transparency_ = -1.f;
+  red_ = -1.f;
+  green_ = -1.f;
+  blue_ = -1.f;
+}
+
+bool OpenGlDrawVisitor::OpenGlStateCache::ColorStateChanged(
+    float actor_opacity, float dimmed_transparency,
+    float red, float green, float blue) {
+  if (actor_opacity != actor_opacity_ ||
+      dimmed_transparency != dimmed_transparency_ ||
+      red != red_ || green != green_ || blue != blue_) {
+    actor_opacity_ = actor_opacity;
+    dimmed_transparency_ = dimmed_transparency;
+    red_ = red;
+    green_ = green;
+    blue_ = blue;
+    return true;
   }
-  texture_ = texture;
-  has_alpha_ = has_alpha;
+  return false;
 }
 
 OpenGlDrawVisitor::OpenGlDrawVisitor(GLInterface* gl_interface,
@@ -309,7 +304,7 @@ void OpenGlDrawVisitor::FindFramebufferConfigurations() {
 OpenGlDrawVisitor::~OpenGlDrawVisitor() {
   gl_interface_->Finish();
   // Make sure the vertex buffer is deleted.
-  quad_drawing_data_ = RealCompositor::DrawingDataPtr();
+  quad_drawing_data_.reset(NULL);
   CHECK_GL_ERROR(gl_interface_);
   gl_interface_->MakeGlxCurrent(0, 0);
   if (context_) {
@@ -318,8 +313,7 @@ OpenGlDrawVisitor::~OpenGlDrawVisitor() {
 }
 
 void OpenGlDrawVisitor::BindImage(const ImageContainer* container,
-                                  RealCompositor::QuadActor* actor) {
-  PROFILER_MARKER_BEGIN(BindImage);
+                                  RealCompositor::ImageActor* actor) {
   // Create an OpenGL texture with the loaded image data.
   GLuint new_texture;
   gl_interface_->Enable(GL_TEXTURE_2D);
@@ -344,18 +338,27 @@ void OpenGlDrawVisitor::BindImage(const ImageContainer* container,
                             container->data());
   CHECK_GL_ERROR(gl_interface_);
   OpenGlTextureData* data = new OpenGlTextureData(gl_interface_);
-  data->SetTexture(new_texture,
-                   container->format() == ImageContainer::IMAGE_FORMAT_RGBA_32);
+  data->SetTexture(new_texture);
+  data->set_has_alpha(
+      container->format() == ImageContainer::IMAGE_FORMAT_RGBA_32);
   actor->SetSize(container->width(), container->height());
-  actor->SetDrawingData(OpenGlDrawVisitor::TEXTURE_DATA,
-                        RealCompositor::DrawingDataPtr(data));
-  DLOG(INFO) << "Binding image " << container->filename()
-             << " to texture " << new_texture;
-  PROFILER_MARKER_END(BindImage);
+  actor->set_texture_data(data);
 }
 
 void OpenGlDrawVisitor::VisitActor(RealCompositor::Actor* actor) {
   // Base actors actually don't have anything to draw.
+}
+
+void OpenGlDrawVisitor::VisitImage(RealCompositor::ImageActor* actor) {
+  if (!actor->IsVisible())
+    return;
+
+  PROFILER_MARKER_BEGIN(VisitImage);
+
+  // All ImageActors are also QuadActors, and so we let the
+  // QuadActor do all the actual drawing.
+  VisitQuad(actor);
+  PROFILER_MARKER_END(VisitImage);
 }
 
 void OpenGlDrawVisitor::VisitTexturePixmap(
@@ -366,19 +369,19 @@ void OpenGlDrawVisitor::VisitTexturePixmap(
   PROFILER_MARKER_BEGIN(VisitTexturePixmap);
 
   // Make sure there's a bound texture.
-  if (!actor->GetDrawingData(PIXMAP_DATA).get()) {
+  if (!actor->texture_data()) {
     if (!actor->pixmap()) {
       PROFILER_MARKER_END(VisitTexturePixmap);
       return;
     }
 
-    scoped_ptr<OpenGlPixmapData> data(new OpenGlPixmapData(this));
+    OpenGlPixmapData* data = new OpenGlPixmapData(this);
     if (!data->Init(actor)) {
       PROFILER_MARKER_END(VisitTexturePixmap);
       return;
     }
-    actor->SetDrawingData(OpenGlDrawVisitor::PIXMAP_DATA,
-                          RealCompositor::DrawingDataPtr(data.release()));
+    data->set_has_alpha(!actor->pixmap_is_opaque());
+    actor->set_texture_data(data);
   }
 
   // All texture pixmaps are also QuadActors, and so we let the
@@ -395,17 +398,6 @@ void OpenGlDrawVisitor::VisitQuad(RealCompositor::QuadActor* actor) {
   DLOG(INFO) << "Drawing quad " << actor->name() << ".";
 #endif
   PROFILER_DYNAMIC_MARKER_BEGIN(actor->name().c_str());
-
-  RealCompositor::DrawingData* generic_drawing_data =
-      actor->GetDrawingData(DRAWING_DATA).get();
-  if (!generic_drawing_data) {
-    // This actor hasn't been here before, so let's set the drawing data on it.
-    actor->SetDrawingData(DRAWING_DATA, quad_drawing_data_);
-    generic_drawing_data = quad_drawing_data_.get();
-  }
-  OpenGlQuadDrawingData* drawing_data =
-      dynamic_cast<OpenGlQuadDrawingData*>(generic_drawing_data);
-  CHECK(drawing_data);
 
   // Calculate the vertex colors, taking into account the actor color,
   // opacity and the dimming gradient.
@@ -436,50 +428,36 @@ void OpenGlDrawVisitor::VisitQuad(RealCompositor::QuadActor* actor) {
     float dim_green = green * dimmed_transparency;
     float dim_blue = blue * dimmed_transparency;
 
-    drawing_data->set_vertex_color(
+    quad_drawing_data_->set_vertex_color(
         0, red, green, blue, actor_opacity);
-    drawing_data->set_vertex_color(
+    quad_drawing_data_->set_vertex_color(
         1, red, green, blue, actor_opacity);
-    drawing_data->set_vertex_color(
+    quad_drawing_data_->set_vertex_color(
         2, dim_red, dim_green, dim_blue, actor_opacity);
-    drawing_data->set_vertex_color(
+    quad_drawing_data_->set_vertex_color(
         3, dim_red, dim_green, dim_blue, actor_opacity);
 
+    gl_interface_->EnableClientState(GL_COLOR_ARRAY);
     // Have to un-bind the array buffer to set the color pointer so that
     // it uses the color buffer instead of the vertex buffer memory.
     gl_interface_->BindBuffer(GL_ARRAY_BUFFER, 0);
-    gl_interface_->ColorPointer(4, GL_FLOAT, 0, drawing_data->color_buffer());
+    gl_interface_->ColorPointer(4, GL_FLOAT, 0,
+                                quad_drawing_data_->color_buffer());
   }
 
-  gl_interface_->BindBuffer(GL_ARRAY_BUFFER, drawing_data->vertex_buffer());
+  gl_interface_->BindBuffer(GL_ARRAY_BUFFER,
+                            quad_drawing_data_->vertex_buffer());
   CHECK_GL_ERROR(gl_interface_);
 
   // Find out if this quad has pixmap or texture data to bind.
-  RealCompositor::DrawingData* generic_pixmap_data =
-      actor->GetDrawingData(PIXMAP_DATA).get();
-  OpenGlPixmapData* pixmap_data =
-      dynamic_cast<OpenGlPixmapData*>(generic_pixmap_data);
-  if (generic_pixmap_data)
-    CHECK(pixmap_data);
-  if (pixmap_data && pixmap_data->texture()) {
-    // Actor has a pixmap texture to bind.
+  if (actor->texture_data()) {
+    // Actor has a texture to bind.
     gl_interface_->Enable(GL_TEXTURE_2D);
-    gl_interface_->BindTexture(GL_TEXTURE_2D, pixmap_data->texture());
+    gl_interface_->BindTexture(GL_TEXTURE_2D,
+                               actor->texture_data()->texture());
   } else {
-    RealCompositor::DrawingData* generic_texture_data =
-        actor->GetDrawingData(TEXTURE_DATA).get();
-    OpenGlTextureData* texture_data =
-        dynamic_cast<OpenGlTextureData*>(generic_texture_data);
-    if (generic_texture_data)
-      CHECK(texture_data);
-    if (texture_data && texture_data->texture()) {
-      // Actor has a texture to bind.
-      gl_interface_->Enable(GL_TEXTURE_2D);
-      gl_interface_->BindTexture(GL_TEXTURE_2D, texture_data->texture());
-    } else {
-      // Actor has no texture.
-      gl_interface_->Disable(GL_TEXTURE_2D);
-    }
+    // Actor has no texture.
+    gl_interface_->Disable(GL_TEXTURE_2D);
   }
 
 #ifdef EXTRA_LOGGING
@@ -502,10 +480,8 @@ void OpenGlDrawVisitor::VisitQuad(RealCompositor::QuadActor* actor) {
 
 void OpenGlDrawVisitor::DrawNeedle() {
   PROFILER_MARKER_BEGIN(DrawNeedle);
-  OpenGlQuadDrawingData* drawing_data =
-      dynamic_cast<OpenGlQuadDrawingData*>(quad_drawing_data_.get());
-  CHECK(drawing_data);
-  gl_interface_->BindBuffer(GL_ARRAY_BUFFER, drawing_data->vertex_buffer());
+  gl_interface_->BindBuffer(GL_ARRAY_BUFFER,
+                            quad_drawing_data_->vertex_buffer());
   gl_interface_->EnableClientState(GL_VERTEX_ARRAY);
   gl_interface_->VertexPointer(2, GL_FLOAT, 0, 0);
   gl_interface_->DisableClientState(GL_TEXTURE_COORD_ARRAY);
@@ -528,9 +504,6 @@ void OpenGlDrawVisitor::VisitStage(RealCompositor::StageActor* actor) {
 
   PROFILER_MARKER_BEGIN(VisitStage);
   stage_ = actor;
-  OpenGlQuadDrawingData* drawing_data =
-      dynamic_cast<OpenGlQuadDrawingData*>(quad_drawing_data_.get());
-  CHECK(drawing_data);
 
   if (actor->stage_color_changed()) {
     const Compositor::Color& color = actor->stage_color();
@@ -564,7 +537,8 @@ void OpenGlDrawVisitor::VisitStage(RealCompositor::StageActor* actor) {
   gl_interface_->LoadMatrixf(&projection[0][0]);
   gl_interface_->MatrixMode(GL_MODELVIEW);
   gl_interface_->LoadIdentity();
-  gl_interface_->BindBuffer(GL_ARRAY_BUFFER, drawing_data->vertex_buffer());
+  gl_interface_->BindBuffer(GL_ARRAY_BUFFER,
+                            quad_drawing_data_->vertex_buffer());
   gl_interface_->EnableClientState(GL_VERTEX_ARRAY);
   gl_interface_->VertexPointer(2, GL_FLOAT, 0, 0);
   gl_interface_->EnableClientState(GL_TEXTURE_COORD_ARRAY);

opengl_visitor.h
@@ -15,108 +15,85 @@
 #include "window_manager/gl_interface.h"
 #include "window_manager/image_container.h"
 #include "window_manager/real_compositor.h"
+#include "window_manager/texture_data.h"
 #include "window_manager/x_connection.h"
 
 namespace window_manager {
 
-class OpenGlDrawVisitor;
-
-class OpenGlQuadDrawingData : public RealCompositor::DrawingData  {
+class OpenGlTextureData : public TextureData {
  public:
-  explicit OpenGlQuadDrawingData(GLInterface* gl_interface);
-  virtual ~OpenGlQuadDrawingData();
+  explicit OpenGlTextureData(GLInterface* gl_interface);
+  virtual ~OpenGlTextureData();
 
-  GLuint vertex_buffer() { return vertex_buffer_; }
-  float* color_buffer() { return color_buffer_.get(); }
-
-  // Sets the vertex color of the give vertex index.
-  // Has no effect on drawing until the next call to SetBufferData.
-  void set_vertex_color(int index, float r, float g, float b, float a);
+  void SetTexture(GLuint texture);
 
  private:
-  // This is the gl interface to use for communicating with GL.
+  // This is the GL interface to use for communicating with GL.
   GLInterface* gl_interface_;
-
-  // This is the vertex buffer that holds the rect we use for
-  // rendering quads.
-  GLuint vertex_buffer_;
-
-  scoped_array<float> color_buffer_;
 };
 
-class OpenGlPixmapData : public RealCompositor::DrawingData  {
+class OpenGlPixmapData : public DynamicTextureData {
  public:
   OpenGlPixmapData(OpenGlDrawVisitor* visitor);
   virtual ~OpenGlPixmapData();
+
+  virtual void Refresh();
 
   // This creates a GLX pixmap from the actor's pixmap and binds it to a GL
   // texture.  false is returned if the process fails (in which case this
   // object should be thrown away).
   bool Init(RealCompositor::TexturePixmapActor* actor);
 
-  // Refresh the texture in response to the X pixmap's contents being
-  // modified.
-  void Refresh();
-
-  GLuint texture() const { return texture_; }
-
  private:
   OpenGlDrawVisitor* visitor_;  // not owned
   GLInterface* gl_;             // not owned
-
-  // GL texture.
-  GLuint texture_;
 
   // GLX pixmap created from the actor's X pixmap.
   GLXPixmap glx_pixmap_;
 };
 
-class OpenGlTextureData : public RealCompositor::DrawingData  {
- public:
-  explicit OpenGlTextureData(GLInterface* gl_interface);
-  virtual ~OpenGlTextureData();
-
-  void SetTexture(GLuint texture, bool has_alpha);
-
-  GLuint texture() const { return texture_; }
-  bool has_alpha() const { return has_alpha_; }
-
- private:
-  // This is the GL interface to use for communicating with GL.
-  GLInterface* gl_interface_;
-
-  // This is the texture ID of the bound texture.
-  GLuint texture_;
-
-  // True if associated texture has an alpha channel.
-  bool has_alpha_;
-};
-
 // This class visits an actor tree and draws it using OpenGL.
 class OpenGlDrawVisitor : virtual public RealCompositor::ActorVisitor {
  public:
-  // These are IDs used when storing drawing data on the actors.
-  enum DataId {
-    TEXTURE_DATA = 1,
-    PIXMAP_DATA = 2,
-    DRAWING_DATA = 3,
-  };
-
   OpenGlDrawVisitor(GLInterface* gl_interface,
                     RealCompositor* compositor,
                     Compositor::StageActor* stage);
   virtual ~OpenGlDrawVisitor();
 
   void BindImage(const ImageContainer* container,
-                 RealCompositor::QuadActor* actor);
+                 RealCompositor::ImageActor* actor);
 
   virtual void VisitActor(RealCompositor::Actor* actor);
   virtual void VisitStage(RealCompositor::StageActor* actor);
   virtual void VisitContainer(RealCompositor::ContainerActor* actor);
+  virtual void VisitImage(RealCompositor::ImageActor* actor);
   virtual void VisitTexturePixmap(RealCompositor::TexturePixmapActor* actor);
   virtual void VisitQuad(RealCompositor::QuadActor* actor);
 
  private:
+  class OpenGlQuadDrawingData {
+   public:
+    explicit OpenGlQuadDrawingData(GLInterface* gl_interface);
+    virtual ~OpenGlQuadDrawingData();
+
+    GLuint vertex_buffer() { return vertex_buffer_; }
+    float* color_buffer() { return color_buffer_.get(); }
+
+    // Sets the vertex color of the give vertex index.
+    // Has no effect on drawing until the next call to SetBufferData.
+    void set_vertex_color(int index, float r, float g, float b, float a);
+
+   private:
+    // This is the gl interface to use for communicating with GL.
+    GLInterface* gl_interface_;
+
+    // This is the vertex buffer that holds the rect we use for
+    // rendering quads.
+    GLuint vertex_buffer_;
+
+    scoped_array<float> color_buffer_;
+  };
+
   class OpenGlStateCache {
    public:
     OpenGlStateCache();
@@ -147,7 +124,7 @@ class OpenGlDrawVisitor : virtual public RealCompositor::ActorVisitor {
   // This holds the drawing data used for quads.  Note that only
   // QuadActors use this drawing data, and they all share the same
   // one (to keep from allocating a lot of quad vertex buffers).
-  RealCompositor::DrawingDataPtr quad_drawing_data_;
+  scoped_ptr<OpenGlQuadDrawingData> quad_drawing_data_;
 
   // The framebuffer configs to use with this display.
   GLXFBConfig framebuffer_config_rgb_;

commit 2f32e22dbc2f08cfc567b8ab8e0af39c29f73e60

wm: Implement VisitImage in OpenGL|ES backend.
- This fixes the background in overview mode.
- This fixes a regression caused by 98b6cb5 'combine texture and pixmap
  prototype'.

TEST=Run on tegra2_seaboard
BUG=None

Review URL: http://codereview.chromium.org/3520002

Change-Id: I9a1fab9722eac1d3282141c3cb720a7debde0ff4

gles/opengles_visitor.h
@@ -46,6 +46,9 @@ class OpenGlesDrawVisitor : virtual public RealCompositor::ActorVisitor {
   virtual void VisitActor(RealCompositor::Actor* actor) {}
   virtual void VisitStage(RealCompositor::StageActor* actor);
   virtual void VisitContainer(RealCompositor::ContainerActor* actor);
+  virtual void VisitImage(RealCompositor::ImageActor* actor) {
+    VisitQuad(actor);
+  }
   virtual void VisitTexturePixmap(RealCompositor::TexturePixmapActor* actor);
   virtual void VisitQuad(RealCompositor::QuadActor* actor);
 