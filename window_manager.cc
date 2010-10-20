joshfire/chromiumos-platform-window_manager

commit 5710ca076ceb4168aec8e7e3d3ddfb3f66c5003c

wm: Update configure-monitor and screenshot key bindings.
This makes us use Ctrl-F4 for reconfiguring the display for
an external display and Ctrl-F5 for taking a screenshot.
(The old bindings for these actions were Ctrl-Alt-M and
Ctrl-F4, respectively.)

It also ifdefs out the Ctrl-Alt-D and Ctrl-Alt-P hotkeys
for non-debug builds; they're debug-type actions that
confuse users who type them accidentally.

Change-Id: Iea3b230e81df49a0f86da22d3bca5f2cc2c3a914

BUG=chromium-os:4108
TEST=updated

Review URL: http://codereview.chromium.org/3926001

window_manager.cc
@@ -1052,6 +1052,7 @@ void WindowManager::RegisterKeyBindings() {
           XK_t, KeyBindings::kControlMask | KeyBindings::kAltMask),
       kLaunchTerminalAction);
 
+#ifndef NDEBUG
   key_bindings_actions_->AddAction(
       kToggleClientWindowDebuggingAction,
       NewPermanentCallback(this, &WindowManager::ToggleClientWindowDebugging),
@@ -1069,6 +1070,7 @@ void WindowManager::RegisterKeyBindings() {
       KeyBindings::KeyCombo(
           XK_p, KeyBindings::kControlMask | KeyBindings::kAltMask),
       kToggleProfilerAction);
+#endif
 
   key_bindings_actions_->AddAction(
       kConfigureMonitorAction,
@@ -1076,8 +1078,7 @@ void WindowManager::RegisterKeyBindings() {
                            FLAGS_configure_monitor_command),
       NULL, NULL);
   key_bindings_->AddBinding(
-      KeyBindings::KeyCombo(
-          XK_m, KeyBindings::kControlMask | KeyBindings::kAltMask),
+      KeyBindings::KeyCombo(XK_F4, KeyBindings::kControlMask),
       kConfigureMonitorAction);
 
   key_bindings_actions_->AddAction(
@@ -1100,7 +1101,7 @@ void WindowManager::RegisterKeyBindings() {
       NewPermanentCallback(this, &WindowManager::TakeScreenshot, false),
       NULL, NULL);
   key_bindings_->AddBinding(
-      KeyBindings::KeyCombo(XK_F4, KeyBindings::kControlMask),
+      KeyBindings::KeyCombo(XK_F5, KeyBindings::kControlMask),
       kTakeRootScreenshotAction);
   key_bindings_->AddBinding(
       KeyBindings::KeyCombo(XK_Print, 0), kTakeRootScreenshotAction);

window_manager_test.cc
@@ -897,16 +897,18 @@ TEST_F(WindowManagerTest, LoggedIn) {
 // MappingNotify event.
 TEST_F(WindowManagerTest, HandleMappingNotify) {
   // Check that a grab has been installed for an arbitrary key binding
-  // (Ctrl-Alt-m).
+  // (Ctrl-F4).
   EXPECT_EQ(0, xconn_->num_keymap_refreshes());
-  const KeyCode old_keycode = xconn_->GetKeyCodeFromKeySym(XK_m);
-  EXPECT_TRUE(xconn_->KeyIsGrabbed(old_keycode, ControlMask | Mod1Mask));
+  const KeySym keysym = XK_F4;
+  const uint32_t modifiers = ControlMask;
+  const KeyCode old_keycode = xconn_->GetKeyCodeFromKeySym(keysym);
+  EXPECT_TRUE(xconn_->KeyIsGrabbed(old_keycode, modifiers));
 
   // Now remap the 'm' key and give the window manager a MappingNotify event.
   const KeyCode new_keycode = 255;
-  EXPECT_FALSE(xconn_->KeyIsGrabbed(new_keycode, ControlMask | Mod1Mask));
-  xconn_->RemoveKeyMapping(old_keycode, XK_m);
-  xconn_->AddKeyMapping(new_keycode, XK_m);
+  EXPECT_FALSE(xconn_->KeyIsGrabbed(new_keycode, modifiers));
+  xconn_->RemoveKeyMapping(old_keycode, keysym);
+  xconn_->AddKeyMapping(new_keycode, keysym);
 
   XEvent event;
   XMappingEvent* mapping_event = &(event.xmapping);
@@ -921,8 +923,8 @@ TEST_F(WindowManagerTest, HandleMappingNotify) {
   // keyboard grab should be updated (there are more-extensive tests of the
   // latter behavior in KeyBindingsTest).
   EXPECT_EQ(1, xconn_->num_keymap_refreshes());
-  EXPECT_TRUE(xconn_->KeyIsGrabbed(new_keycode, ControlMask | Mod1Mask));
-  EXPECT_FALSE(xconn_->KeyIsGrabbed(old_keycode, ControlMask | Mod1Mask));
+  EXPECT_TRUE(xconn_->KeyIsGrabbed(new_keycode, modifiers));
+  EXPECT_FALSE(xconn_->KeyIsGrabbed(old_keycode, modifiers));
 }
 
 // Check that the window manager tells the Window class to tell the

commit f498f49163e3cf86a17666657f1cbb0155e9747d

wm: Fix non-debug build breakage from 5710ca07.
I left some variables that were needed for debug builds, but
they caused "defined but not used" warnings when NDEBUG was
defined.

Change-Id: I13ec6b0752af868d8ef152b9001ded477109b86e

BUG=none
TEST=ran build_packages --nowithdebug
TBR=zelidrag

Review URL: http://codereview.chromium.org/3875002

window_manager.cc
@@ -114,9 +114,11 @@ COMPILE_ASSERT(kPingChromeFrequencyMs > kPingChromeTimeoutMs,
 
 // Names of key binding actions that we register.
 static const char* kLaunchTerminalAction = "launch-terminal";
+#ifndef NDEBUG
 static const char* kToggleClientWindowDebuggingAction =
     "toggle-client-window-debugging";
 static const char* kToggleProfilerAction = "toggle-profiler";
+#endif
 static const char* kConfigureMonitorAction = "configure-monitor";
 static const char* kToggleHotkeyOverlayAction = "toggle-hotkey-overlay";
 static const char* kTakeRootScreenshotAction = "take-root-screenshot";